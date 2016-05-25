f4ed02e5aa3347cacac2896cf8993d745f1a20d1
Captain <captain@fascinatingcaptain.com>
2016-05-25 14:20:33 +0100

    Update CBFixesAndTweaks.sh

--- CBFixesAndTweaks.sh
+++ CBFixesAndTweaks.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo 'hello there'
+# stop sound service
+sudo alsa force-unload
+
+# make backup of original sound.state file
+sudo cp /var/lib/alsa/asound.state /var/lib/alsa/asound.state.bck
+
+# cp new sound.state file
+sudo cp asound.state /var/lib/alsa
