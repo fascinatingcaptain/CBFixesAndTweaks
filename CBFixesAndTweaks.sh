2a5906d9de72bc2604577d487a9eda6e38391eca
Captain <captain@fascinatingcaptain.com>
2016-05-25 14:55:50 +0100

    Update CBFixesAndTweaks.sh

--- CBFixesAndTweaks.sh
+++ CBFixesAndTweaks.sh
@@ -4,7 +4,7 @@
 sudo alsa force-unload
 
 # make backup of original sound.state file
-sudo cp /var/lib/alsa/asound.state /var/lib/alsa/asound.state.bck
+sudo cp -n /var/lib/alsa/asound.state /var/lib/alsa/asound.state.bck
 
 # cp new sound.state file
 sudo cp asound.state /var/lib/alsa
