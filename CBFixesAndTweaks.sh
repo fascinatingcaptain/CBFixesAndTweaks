d024c502556d3a356c85bdb5cbcd93f1ae613673
Captain <captain@fascinatingcaptain.com>
2016-05-31 12:58:41 +0100

    Update CBFixesAndTweaks.sh
    added keyboard fix

--- CBFixesAndTweaks.sh
+++ CBFixesAndTweaks.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+## fix sound
 # stop sound service
 sudo alsa force-unload
 
 # make backup of original sound.state file
 sudo cp -n /var/lib/alsa/asound.state /var/lib/alsa/asound.state.bck
 
-# cp new sound.state file
+# copy new sound.state file
 sudo cp asound.state /var/lib/alsa
+
+## fix keyboard
+# make backup of original pc config file
+sudo cp -n /usr/share/X11/xkb/symbols/pc /usr/share/X11/xkb/symbols/pc.bck
+
+# copy new pc config file
+sudo cp pc /usr/share/X11/xkb/symbols/
+
+# update config
+sudo rm -rf /var/lib/xkb/*
