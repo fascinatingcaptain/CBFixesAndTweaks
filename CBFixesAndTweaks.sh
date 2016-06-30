7b2f2de64bd9d81d6a5bfe4d8d4bafb03621bc15
Captain <captain@fascinatingcaptain.com>
2016-06-30 10:24:50 +0100

    Put sound fix in function
    So that other Chromebooks can be supported as well.

--- CBFixesAndTweaks.sh
+++ CBFixesAndTweaks.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-## fix sound
-# stop sound service
-sudo alsa force-unload
+## need to check if this is Baytrail TCB2
+# sudo dmidecode -s system-product-name
 
-# make backup of original sound.state file
-sudo cp -n /var/lib/alsa/asound.state /var/lib/alsa/asound.state.bck
-
-# copy new sound.state file
-sudo cp asound.state /var/lib/alsa
+## will need to test when I get home
+fix_sound() {
+  ## fix sound
+  # stop sound service
+  sudo alsa force-unload
+  
+  # make backup of original sound.state file
+  sudo cp -n /var/lib/alsa/asound.state /var/lib/alsa/asound.state.bck
+  
+  # copy new sound.state file
+  sudo cp asound.state /var/lib/alsa
+}
 
 ## fix keyboard
 # make backup of original pc config file
